317e6ec357e62bac688fe5c93f45bf4e7c2629ad
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-04-20 01:29:27 +0900

    Add journal03

---
+++ journal03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using journal.console.lib.Console;
+using kj.kihon;
+
+namespace journal03
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var log = new ErrorLogger("{AppUtil.AppName }:Word/一太郎からテキスト変換");
+      try
+      {
+        var lst = ArgListUtil.createArgList(args);
+        var srcdir = lst.getText('i');
+        srcdir.existDir();
+
+        var core = new journal03_util(log);
+        core.Run(srcdir);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("エラーがあります {0}", ex.Message);
+      }
+      log.showdosmsg();
+    }
+  }
+}
