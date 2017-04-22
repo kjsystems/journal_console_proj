fae9ee2bfbb91717c5009e879907ed0ff0bf01e7
satoru-yoga <satoru.fujimori@gmail.com>
2017-04-22 13:20:11 +0900

    Update

--- journal.Test/TestQurey.cs
+++ journal.Test/TestQurey.cs
@@ -13,17 +13,15 @@ namespace journal.Test
     public void TestQureyHonbun_One()
     {
       var ser = new SearchManager();
-      ser.CreateSearchIndexClient();
       var result = ser.QueryHonbun(new[] { "藤原定家" }.ToList());
-      Assert.AreEqual(50, result.Count);
+      Assert.AreEqual(9, result.Count);
     }
     [Test]
     public void TestQureyHonbun_And()
     {
       var ser = new SearchManager();
-      ser.CreateSearchIndexClient();
       var result = ser.QueryHonbun(new[] { "藤原定家","古今和歌集" }.ToList());
-      Assert.AreEqual(6, result.Count);
+      Assert.AreEqual(3, result.Count);
       Assert.AreEqual("001-03", result[0].FileName);
       Assert.AreEqual("・同1991『藤原定家筆古今和歌集』汲古書院", result[0].Text);
     }
@@ -31,9 +29,8 @@ namespace journal.Test
     public void TestQureyHonbun_Or()
     {
       var ser = new SearchManager();
-      ser.CreateSearchIndexClient();
       var result = ser.QueryHonbun(new[] { "藤原定家", "古今和歌集" }.ToList(), SearchManager.JournalSearchMode.Any);
-      Assert.AreEqual(50, result.Count);
+      Assert.AreEqual(30, result.Count);
     }
   }
 }
