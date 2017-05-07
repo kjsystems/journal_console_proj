4746803dc8f6aca91e16707b5660762a7c1cbd79
Storu Fujimori <satoru.fujimori@gmail.com>
2017-05-07 12:44:16 +0900

    Add journal06

---
+++ journal.console.lib/Consoles/journal06_util.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using kj.kihon;
+
+namespace journal.console.lib.Consoles
+{
+  public class journal06_util : kihon_base
+  {
+    public journal06_util(ILogger log) : base(log)
+    {
+    }
+
+    public void Run(string jobdir)
+    {
+      jobdir.existDir();
+      var txtdir = jobdir.combine("txt");
+      txtdir.existDir();
+      var kjpdir = jobdir.combine("kjp").createDirIfNotExist();
+
+      foreach (var srcpath in txtdir.getFiles("*.txt"))
+      {
+        var outpath = kjpdir.combine(srcpath.getFileNameWithoutExtension() + ".kjp");
+        System.Console.WriteLine($"{srcpath}");
+        System.Console.WriteLine($"==>{outpath}");
+
+        FileUtil.writeTextToFile(CreateTextFromPath(srcpath),Encoding.UTF8, outpath);
+      }
+    }
+
+    string CreateTextFromPath(string path)
+    {
+      var util=new TagTextUtil(Log);
+      var taglst = util.parseTextFromPath(path, Encoding.UTF8);
+
+      var sb=new StringBuilder();
+      foreach (TagBase tag in taglst)
+      {
+        if (tag.isText())
+        {
+          var txt = tag.ToString()
+            .Replace("", "&#x3033;")
+            .Replace("α", "&#x3033;&#x3035;")
+            .Replace("&#12349;", "ヽ")
+            .Replace("", "&#x3035;")
+            .Replace("", "&#x303b;")
+            .Replace("β", "&#x303b;")
+            .Replace("￥", "")
+            .Replace("$", "＄")
+            .Replace("＄", "&#x25e6;");
+          sb.Append(txt);
+          continue;
+        }
+        sb.Append(tag.ToString());
+      }
+      return sb.ToString();
+    }
+  }
+}
---
+++ journal06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using journal.console.lib.Consoles;
+using kj.kihon;
+
+namespace journal06
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var log = new ErrorLogger("{AppUtil.AppName }:入稿テキストをKJPへ変換");
+      try
+      {
+        var lst = ArgListUtil.createArgList(args);
+        var srcdir = lst.getText('i');
+        srcdir.existDir();
+
+        var core = new journal06_util(log);
+        core.Run(srcdir);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("エラーがあります {0}", ex.Message);
+      }
+      log.showdosmsg();
+    }
+  }
+}
