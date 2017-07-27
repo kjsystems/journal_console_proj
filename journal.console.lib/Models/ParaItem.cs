1b9cd8ae108db5913efc7a400fa3502671bd9756
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-07-27 22:11:21 +0900

    Update ParaItem for journal06

--- journal.console.lib/Models/ParaItem.cs
+++ journal.console.lib/Models/ParaItem.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace journal.console.lib.Models
 {
-  public class ParaItem
-  {
-    public int Gyo { get; set; }
-    public string Text { get; set; }
-  }
+    public class ParaItem
+    {
+        public int Gyo { get; set; }
+        public string Text { get; set; }
+
+        public int Jisage { get; set; } = 0;
+        public int Mondo { get; set; } = 0;
+        public bool IsJisoroe { get; set; } = false;
+    }
 }
