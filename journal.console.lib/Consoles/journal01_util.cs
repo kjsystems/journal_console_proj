2bfd0d520d85ca8595e609467de7a394d73deb08
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-06 14:23:19 +0900

    Add journal01(txt→xml,kenkyu02から移動)

---
+++ journal.console.lib/Consoles/journal01_util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using journal.console.lib.Models;
+using kj.kihon;
+using Microsoft.VisualBasic.Logging;
+
+namespace journal.console.lib.Consoles
+{
+    public class journal01_util : kihon_base
+    {
+        public void RunText2Xml(string srcdir)
+        {
+            var txtdir = srcdir.combine("txt");
+            var xmldir = srcdir.combine("xml").createDirIfNotExist();
+            string[] srclst = txtdir.getFiles("*.txt");
+            foreach (var srcpath in srclst)
+            {
+                var outpath = xmldir.combine(srcpath.getFileNameWithoutExtension()+".xml");
+                Console.WriteLine($"==>{outpath}");
+                //XMLへ出力
+                CreateWakaXmlCore core = new CreateWakaXmlCore(Log);
+                core.run(srcpath, outpath);
+            }
+
+        }
+
+        public journal01_util(ILogger log) : base(log)
+        {
+        }
+    }
+}
