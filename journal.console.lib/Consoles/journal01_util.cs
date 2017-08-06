c90e7ed07298c8bd8da2475eba7317e28d28420f
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-06 14:52:57 +0900

    journal01 ファイル名を変更（02-01gomihumihiko.txt→002-01）

--- journal.console.lib/Consoles/journal01_util.cs
+++ journal.console.lib/Consoles/journal01_util.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using journal.console.lib.Models;
 using kj.kihon;
 using Microsoft.VisualBasic.Logging;
@@ -14,13 +15,29 @@ namespace journal.console.lib.Consoles
             string[] srclst = txtdir.getFiles("*.txt");
             foreach (var srcpath in srclst)
             {
-                var outpath = xmldir.combine(srcpath.getFileNameWithoutExtension()+".xml");
+                // ファイル名を変換する
+                // 02-01gomihumihiko.xml ==> 002-01 rename
+                var fname = ChangeFileName( srcpath.getFileNameWithoutExtension());
+                
+                var outpath = xmldir.combine(fname + ".xml");
                 Console.WriteLine($"==>{outpath}");
                 //XMLへ出力
                 CreateWakaXmlCore core = new CreateWakaXmlCore(Log);
                 core.run(srcpath, outpath);
             }
 
+        }
+
+        // 02-01gomihumihiko.xml ==> 002-01
+        string ChangeFileName(string fname)
+        {
+            var match = Regex.Match(fname,@"^([0-9]{2}-[0-9]{2})");
+            if (match != null)
+            {
+                var grp = RegexUtil.getGroup(match, 1);
+                return $"0{grp}";
+            }
+            return fname;
         }
 
         public journal01_util(ILogger log) : base(log)
