ea8e9028806ac0d8a5b0db84ae8f0115fe84578b
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-06 15:52:09 +0900

    Update

--- journal04/Program.cs
+++ journal04/Program.cs
@@ -8,25 +8,26 @@ using journal.console.lib.Consoles;
 
 namespace journal04
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var log = new ErrorLogger("{AppUtil.AppName }:xmlから抽出");
-      try
-      {
-        var lst = ArgListUtil.createArgList(args);
-        var srcdir = lst.getText('i');
-        srcdir.existDir();
+        static void Main(string[] args)
+        {
+            var log = new ErrorLogger("$error.txt");
+            try
+            {
+                Console.WriteLine($"{AppUtil.AppName }:xmlから抽出");
+                var lst = ArgListUtil.createArgList(args);
+                var srcdir = lst.getText('i');
+                srcdir.existDir();
 
-        var core = new journal04_util(log);
-        core.Run(srcdir);
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine("エラーがあります {0}", ex.Message);
-      }
-      log.showdosmsg();
+                var core = new journal04_util(log);
+                core.Run(srcdir);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("エラーがあります {0}", ex.Message);
+            }
+            log.showdosmsg();
+        }
     }
-  }
 }
