8212db965b3f8922d8ee1f8c47af3e3e9c90de6c
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-07 12:34:37 +0900

    Update journal01→journal09

---
+++ journal09/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using journal.console.lib.Consoles;
+using kj.kihon;
+
+namespace journal09
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var log = new ErrorLogger("$error.txt");
+            if (log.isValid() != true) return;
+            try
+            {
+                Console.WriteLine($"{AppUtil.AppName}:入稿テキストからXMLの作成");
+                var lst = ArgListUtil.createArgList(args);
+                var srcdir = lst.getText('i');
+                srcdir.existDir();
+
+                Console.WriteLine("※TXTからXMLの作成のみ（研究Ｌのsql等は作成しない）");
+                var core = new journal09_util(log);
+                //core.run(srcdir);
+                core.RunText2Xml(srcdir);
+            }
+            catch (Exception ex)
+            {
+                log.err("journal09", ex.Message);
+            }
+            log.showdosmsg();
+        }
+    }
+}
