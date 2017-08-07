4486634acef8e13638928b1feaee1d2358600e5b
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-07 12:56:12 +0900

    Update mokuji.json

--- journal.console.lib/Consoles/journal05_util.cs
+++ journal.console.lib/Consoles/journal05_util.cs
@@ -6,61 +6,73 @@ using kj.kihon;
 
 namespace journal.console.lib.Consoles
 {
-  public class journal05_util : kihon_base
-  {
-
-    public journal05_util(ILogger log) : base(log)
+    public class journal05_util : kihon_base
     {
-    }
+        public journal05_util(ILogger log) : base(log)
+        {
+        }
 
-    #region index/index.jsonを読みこんでAzure Searchに登録
-    public void Run(string srcdir)
-    {
-      srcdir.existDir();
-      var idxdir = srcdir.combine("index");
-      idxdir.existDir();
+        #region index/index.jsonを読みこんでAzure Searchに登録
 
-      //contents.jsonAzure Blobに登録
-      var conpath = idxdir.combine(journal04_util.FILENAME_CONTENTS_JSON);
-      UploadContentsJson(conpath);
+        public void Run(string srcdir)
+        {
+            srcdir.existDir();
+            var idxdir = srcdir.combine("index");
+            idxdir.existDir();
 
-      //Azure Searchにインデックスを登録
-      var idxpath = idxdir.combine(journal04_util.FILENAME_INDEX_JSON);
-      idxpath.existFile();
-      CreateAzureSearchIndex(idxpath);
-    }
-    #endregion
+            //contents.jsonAzure Blobに登録
+            var conpath = idxdir.combine(journal04_util.FILENAME_CONTENTS_JSON);
+            UploadContentsJson(conpath);
 
-    #region Azure Blobにアップロードする
-    void UploadContentsJson(string conpath)
-    {
-      Console.WriteLine("{0}", "Upload json to Blob ...");
-      Console.WriteLine("{0}", conpath);
-      var az = new AzureBlobUtil();
+            var mokupath = idxdir.combine(journal04_util.FILENAME_MOKUJI_JSON);
+            UploadContentsJson(mokupath);
+
+            //Azure Searchにインデックスを登録
+            var idxpath = idxdir.combine(journal04_util.FILENAME_INDEX_JSON);
+            idxpath.existFile();
+            CreateAzureSearchIndex(idxpath);
+        }
+
+        #endregion
+
+        #region Azure Blobにアップロードする
+
+        void UploadContentsJson(string conpath)
+        {
+            Console.WriteLine("{0}", "Upload json to Blob ...");
+            Console.WriteLine("{0}", conpath);
+            var az = new AzureBlobUtil();
+#if DEBUG
+            az.CreateBlobClient("LocalConnectionString");
+#else
       az.CreateBlobClient("webtoshoConnectionString");
-      var container = az.CreateDirectory("journal");
-      az.UploadFile(container,conpath);
+#endif
+            var container = az.CreateDirectory("journal");
+            az.UploadFile(container, conpath);
+        }
+
+        #endregion
+
+        #region Azure Searchにインデックスを登録
+
+        private void CreateAzureSearchIndex(string idxpath)
+        {
+            var ser = new AzureSearchCreateIndex();
+
+            const string tblname = "journalhonbuns";
+            string searchServiceName = "webtosho-journal";
+            string adminApiKey = "64790FEA0044163D5F55563D54D1B73E";
+
+            //Indexを作成する
+            ser.InitSearchServiceClient(adminApiKey, searchServiceName);
+            ser.DeleteIndexIfExists(tblname);
+            ser.CreateIndex<BunshoResult>(tblname);
+
+            //JSONを登録する
+            ser.InitIndexClient(tblname);
+            ser.UploadDocuments<BunshoResult>(idxpath);
+        }
+
+        #endregion
     }
-    #endregion
-
-    #region Azure Searchにインデックスを登録
-    private void CreateAzureSearchIndex(string idxpath)
-    {
-      var ser = new AzureSearchCreateIndex();
-
-      const string tblname = "journalhonbuns";
-      string searchServiceName = "webtosho-journal";
-      string adminApiKey = "64790FEA0044163D5F55563D54D1B73E";
-
-      //Indexを作成する
-      ser.InitSearchServiceClient(adminApiKey, searchServiceName);
-      ser.DeleteIndexIfExists(tblname);
-      ser.CreateIndex<BunshoResult>(tblname);
-
-      //JSONを登録する
-      ser.InitIndexClient(tblname);
-      ser.UploadDocuments<BunshoResult>(idxpath);
-    }
-    #endregion
-  }
 }
