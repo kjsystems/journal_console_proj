1cb901bec53e64d13312f922c52627697467daae
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-20 13:58:26 +0900

    Clean indent

--- journal.console.lib/Consoles/journal03_util.cs
+++ journal.console.lib/Consoles/journal03_util.cs
@@ -9,91 +9,94 @@ using wordxml.Models;
 
 namespace journal.console.lib.Consoles
 {
-  public class journal03_util : kihon_base
-  {
-    public List<ParaItem> ParaList { get; set; }
-
-    public journal03_util(ILogger log) : base(log)
+    public class journal03_util : kihon_base
     {
-      ParaList = new List<ParaItem>();
-    }
+        public List<ParaItem> ParaList { get; set; }
 
-    public void Run(string jobdir)
-    {
-      jobdir.existDir();
-      var xmldir = jobdir.combine("xml");
-      xmldir.existDir();
-      var htmdir = jobdir.combine("html");
-      htmdir.createDirIfNotExist();
-
-      foreach (var xmlpath in xmldir.getFiles("*.xml"))
-      {
-        System.Console.WriteLine(xmlpath);
-        ParseDocumentXml(xmlpath);
-        var txt = CreateHtmlText();
-        var outpath = htmdir.combine(xmlpath.getFileNameWithoutExtension()+".htm");
-        System.Console.WriteLine($"==>{outpath}");
-        FileUtil.writeTextToFile(txt,Encoding.UTF8,outpath);
-      }
-    }
-
-    string CreateHtmlText()
-    {
-      var sb=new StringBuilder();
-      sb.AppendLine("<html>");
-      foreach (var para in ParaList)
-      {
-        sb.AppendLine($"<p>{para.Text}</p>");
-      }
-      sb.AppendLine("</html>");
-      return sb.ToString();
-    }
-
-    #region <w:t>の中のテキスト
-    string ParseRun(XmlNode node)
-    {
-      var sb = new StringBuilder();
-      if (node.NodeType == XmlNodeType.Text)
-      {
-        sb.Append(node.Value);
-        return sb.ToString();
-      }
-      sb.Append(node.OuterXml);
-      return sb.ToString();
-    }
-    #endregion
-
-    #region <文章>
-    void ParseBunsho(XmlNode bunsho)
-    {
-      //runは複数
-      var sb = new StringBuilder();
-      foreach (XmlNode run in bunsho.ChildNodes)  //<w:r>
-      {
-        sb.Append(ParseRun(run));
-      }
-      ParaList.Last().Text = sb.ToString();
-    }
-    #endregion
-
-    #region Parse Xml Path
-    public void ParseDocumentXml(string documentPath)
-    {
-      var sb = new StringBuilder();
-      using (var rd = XmlReader.Create(documentPath))
-      {
-        XmlDocument doc = new XmlDocument();
-        doc.Load(rd);
-        XmlNode root = doc.DocumentElement;
-
-        foreach (XmlNode node in root.ChildNodes)
+        public journal03_util(ILogger log) : base(log)
         {
-          if (node.Name != "文章") continue;
-          ParaList.Add(new ParaItem());
-          ParseBunsho(node);
+            ParaList = new List<ParaItem>();
         }
-      }
+
+        public void Run(string jobdir)
+        {
+            jobdir.existDir();
+            var xmldir = jobdir.combine("xml");
+            xmldir.existDir();
+            var htmdir = jobdir.combine("html");
+            htmdir.createDirIfNotExist();
+
+            foreach (var xmlpath in xmldir.getFiles("*.xml"))
+            {
+                System.Console.WriteLine(xmlpath);
+                ParseDocumentXml(xmlpath);
+                var txt = CreateHtmlText();
+                var outpath = htmdir.combine(xmlpath.getFileNameWithoutExtension() + ".htm");
+                System.Console.WriteLine($"==>{outpath}");
+                FileUtil.writeTextToFile(txt, Encoding.UTF8, outpath);
+            }
+        }
+
+        string CreateHtmlText()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("<html>");
+            foreach (var para in ParaList)
+            {
+                sb.AppendLine($"<p>{para.Text}</p>");
+            }
+            sb.AppendLine("</html>");
+            return sb.ToString();
+        }
+
+        #region <w:t>の中のテキスト
+        string ParseRun(XmlNode node)
+        {
+            var sb = new StringBuilder();
+            if (node.NodeType == XmlNodeType.Text)
+            {
+                sb.Append(node.Value);
+                return sb.ToString();
+            }
+            sb.Append(node.OuterXml);
+            return sb.ToString();
+        }
+        #endregion
+
+        #region <文章>
+        void ParseBunsho(XmlNode bunsho)
+        {
+            //runは複数
+            var sb = new StringBuilder();
+            foreach (XmlNode run in bunsho.ChildNodes)  //<w:r>
+            {
+                sb.Append(ParseRun(run));
+            }
+            ParaList.Last().Text = sb.ToString();
+        }
+        #endregion
+
+        #region Parse Xml Path
+        public void ParseDocumentXml(string documentPath)
+        {
+            var sb = new StringBuilder();
+            using (var rd = XmlReader.Create(documentPath))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(rd);
+                XmlNode root = doc.DocumentElement;
+
+                foreach (XmlNode node in root.ChildNodes)
+                {
+                    if (node.Name == "文章")
+                    {
+                        ParaList.Add(new ParaItem());
+                        ParseBunsho(node);
+
+                    }
+                }
+            }
+        }
+        #endregion
     }
-    #endregion
-  }
 }
