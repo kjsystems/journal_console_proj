4612d2f5e70f21b07b8868c507aee5a9a5013701
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-20 13:45:38 +0900

    Add journal11_util.cs

---
+++ journal.console.lib/Consoles/journal11_util.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using journal.console.lib.Models;
+using kj.kihon;
+
+namespace journal.console.lib.Consoles
+{
+    public class journal11_util : kihon_base
+    {
+        public journal11_util(ILogger log) : base(log)
+        {
+        }
+
+        string CreateTextFromParaList(List<ParaItem> paralst)
+        {
+            var sb=new StringBuilder();
+            foreach (var para in paralst)
+            {
+                sb.AppendLine($"{para.Text}<改行>");
+            }
+            return sb.ToString();
+        }
+
+        public void Run(string srcdir)
+        {
+            srcdir.existDir();
+            var xmldir = srcdir.combine("srcxml");
+            xmldir.existDir();
+            var outdir = srcdir.combine( "txt").createDirIfNotExist();
+
+            string[] srcfiles = xmldir.getFiles("*.xml");
+            foreach (var path in srcfiles)
+            {
+                Console.WriteLine($"{path}");
+                // XMLの読み込み
+                var jo03 = new journal03_util(Log);
+                jo03.ParseDocumentXml(path);
+
+                // TXTへ書き出し
+                var outpath = outdir.combine(path.getFileNameWithoutExtension()+".txt");
+                var txt = CreateTextFromParaList(jo03.ParaList);
+
+                Console.WriteLine($"==>{outpath}");
+                FileUtil.writeTextToFile(txt,Encoding.UTF8,outpath);
+            }
+        }
+    }
+}
