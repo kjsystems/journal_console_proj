afef648d333d956a4e04fd099c3adc7073f01cab
Storu Fujimori <satoru.fujimori@gmail.com>
2017-10-19 13:24:44 +0900

    Add journal12 console

---
+++ journal12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using journal.console.lib.Consoles;
+using kj.kihon;
+
+namespace journal12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var log = new ErrorLogger();
+            try
+            {
+                Console.WriteLine($"KJPからテキストの作成");
+                var lst = ArgListUtil.createArgList(args);
+                var srcdir= lst.getText('i');
+                srcdir.existDir();
+                
+                var util = new journal12_util(log);
+                util.Run(srcdir);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            log.showdosmsg();
+        }
+    }
+}
