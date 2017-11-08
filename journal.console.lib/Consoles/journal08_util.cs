588a5d4e816f58a52c6105b64ca4500b49b30c0b
Storu Fujimori <satoru.fujimori@gmail.com>
2017-11-08 18:44:57 +0900

    Add journal08_util kjpからオプション、kjp-sjisオプションを追加

---
+++ journal.console.lib/Consoles/journal08_util.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using idmltool.shared;
+using kj.kihon;
+using kjp2idml.lib;
+
+namespace journal.console.lib.Consoles
+{
+    public class journal08_util : kihon_base
+    {
+        InddTool _inddTool;
+        public journal08_util(ILogger log) : base(log)
+        {
+        }
+
+        void InitApp()
+        {
+            _inddTool = new InddTool();
+            _inddTool.StartApp();
+        }
+
+        void FinishApp()
+        {
+            _inddTool.FinishApp();
+        }
+        
+
+        // ディレクトリで処理
+        public void RunForJobDir(string srcdir, string templateFileName, bool fromKjp,bool fromSjisKjp, ErrorLogger log)
+        {
+            var kjpdir = srcdir.combine("kjp");
+
+            if (fromSjisKjp == true)
+            {
+                kjpdir.createDirIfNotExist();
+                var sjisdir = srcdir.combine("kjp-sjis");
+                sjisdir.existDir();
+                foreach (var txtpath in sjisdir.getFiles("*.kjp"))
+                {
+                    Console.WriteLine($"{txtpath}");
+                    var outpath = kjpdir.combine(txtpath.getFileName());
+                    Console.WriteLine($"==>{outpath}");
+
+                    var sb = new StringBuilder();
+                    sb.Append(CharUtil.sjis2utf(FileUtil.getTextFromPath(txtpath,EncodingUtil.SJIS)));
+                    FileUtil.writeTextToFile(sb.ToString(),Encoding.UTF8,outpath);
+                }
+            }
+            
+            
+            // TXTはSKIP, KJPから全部処理
+            if (fromKjp == true)
+            {
+                kjpdir.existDir();
+                foreach (var txtpath in kjpdir.getFiles("*.kjp"))
+                {
+                    RunForTextPath(txtpath, templateFileName, fromKjp, log);
+                }
+                return;
+            }
+
+            // TXTから全部処理
+            var txtdir = srcdir.combine("txt");
+            txtdir.existDir();
+            foreach (var txtpath in txtdir.getFiles("*.txt"))
+            {
+                RunForTextPath(txtpath, templateFileName, fromKjp, log);
+            }
+        }
+
+        // ファイル1個処理
+        public void RunForTextPath(string txtpath, string templateFileName, bool fromKjp, ErrorLogger log)
+        {
+            var srcdir = txtpath.getDirectoryName().getUpDir();
+            var kjpdir = srcdir.combine("kjp");
+            var kjppath = kjpdir.combine(txtpath.getFileNameWithoutExtension() + ".kjp");
+
+            if (fromKjp != true)
+            {
+                Console.WriteLine("TXTからKJPの作成");
+                new journal06_util(log).RunFromPath(txtpath, kjppath);
+                if (log.ErrorCount > 0)
+                {
+                    throw new Exception("journal08 中断します");
+                }
+            }
+
+            Console.WriteLine("KJPからIDMLの作成");
+            var kjp2idml = new CreateIdmlFromKjpFile(log);
+            var idmlTemplatePath = srcdir
+                .combine("template")
+                .combine(templateFileName);
+            kjp2idml.InitTemplate(idmlTemplatePath);
+
+            var idmlPath = srcdir.combine("outidml").combine($"{txtpath.getFileNameWithoutExtension()}.idml");
+            kjp2idml.RunFromKjpPath(kjppath, srcdir, idmlPath);
+
+            InitApp();
+            Console.WriteLine("IDMLからINDDの作成");
+            var inddPath = srcdir.combine("indd").combine($"{txtpath.getFileNameWithoutExtension()}.indd");
+            _inddTool.SaveAsIndd(idmlPath, inddPath);
+
+            Console.WriteLine("INDDからPDFの作成");
+            var pdfPath = srcdir.combine("pdf").combine($"{txtpath.getFileNameWithoutExtension()}.pdf");
+            _inddTool.ExportPdfFromIndd(inddPath, "" /*preset*/, pdfPath);
+
+            FinishApp();
+        }
+    }
+}
--- journal08/Program.cs
+++ journal08/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using idmltool.shared;
 using journal.console.lib.Consoles;
 using kj.kihon;
-using kjp2idml.lib;
+using Microsoft.VisualBasic.Logging;
 
 namespace journal08
 {
@@ -19,18 +18,31 @@ namespace journal08
                 
                 // ディレクトリで処理
                 var srcdir = lst.getText('i');
+
+                // kjpから(TXT→KJPをSKIP)
+                var fromKjp = lst.getText('k').toInt(0)>0 ? true : false;
+
+                // kjpから(TXT→KJPをSKIP)
+                var fromSjisKjp = lst.getText('s').toInt(0)>0 ? true : false;
+                
+                var templateFileName = !string.IsNullOrEmpty(lst.getText('t')) ? lst.getText('t') : "template.idml";
+                
+                var util=new journal08_util(log);
                 if (srcdir.existDir(false))
                 {
-                    RunForJobDir(srcdir,log);
+                    util.RunForJobDir(srcdir,templateFileName,fromKjp,fromSjisKjp,log);
                 }
 
                 // テキスト単体で処理
                 var txtpath = lst.getText('e');
                 if (txtpath.existFile(false))
                 {
-                    RunForTextPath(txtpath,log);
+                    if (fromSjisKjp)
+                    {
+                        throw new Exception("sjisのファイル単体オプションは無効");
+                    }
+                    util.RunForTextPath(txtpath,templateFileName,fromKjp,log);
                 }
-
             }
             catch (Exception ex)
             {
@@ -38,61 +50,5 @@ namespace journal08
             }
             log.showdosmsg();
         }
-
-        static void RunForJobDir(string srcdir, ErrorLogger log)
-        {
-            Console.WriteLine("TXTからKJPの作成");
-            new journal06_util(log).Run(srcdir);
-            if (log.ErrorCount>0)
-            {
-                throw new Exception("journal08 中断します");
-            }
-
-            Console.WriteLine("KJPからIDMLの作成");
-            new CreateIdmlFromKjpFile(log).Run(srcdir, "template.idml");
-
-            var inddTool =new InddTool();
-            inddTool.StartApp();
-            
-            Console.WriteLine("IDMLからINDDの作成");
-            inddTool.SaveAsIndd(new []{$"{srcdir}\\outidml"});
-
-            Console.WriteLine("INDDからPDFの作成");
-            inddTool.ExportPdfFromIndd(new[] { $"/i{srcdir}\\indd" });
-        }
-
-        static void RunForTextPath(string txtpath, ErrorLogger log)
-        {
-
-            var srcdir = txtpath.getDirectoryName().getUpDir();
-            var kjpdir = srcdir.combine("kjp");
-            var kjppath = kjpdir.combine(txtpath.getFileNameWithoutExtension()+".kjp");
-            
-            Console.WriteLine("TXTからKJPの作成");
-            new journal06_util(log).RunFromPath(txtpath,kjppath);
-            if (log.ErrorCount>0)
-            {
-                throw new Exception("journal08 中断します");
-            }
-
-            Console.WriteLine("KJPからIDMLの作成");
-            var kjp2idml = new CreateIdmlFromKjpFile(log);
-            var idmlTemplatePath = srcdir.combine("template").combine("template.idml");
-            kjp2idml.InitTemplate(idmlTemplatePath);
-
-            var idmlPath = srcdir.combine("outidml").combine($"{txtpath.getFileNameWithoutExtension()}.idml");
-            kjp2idml.RunFromKjpPath(kjppath,srcdir, idmlPath);
-
-            Console.WriteLine("IDMLからINDDの作成");
-            var inddTool =new InddTool();
-            inddTool.StartApp();
-            var inddPath = srcdir.combine("indd").combine($"{txtpath.getFileNameWithoutExtension()}.indd");
-            inddTool.SaveAsIndd(idmlPath, inddPath);
-
-            Console.WriteLine("INDDからPDFの作成");
-            var pdfPath = srcdir.combine("pdf").combine($"{txtpath.getFileNameWithoutExtension()}.pdf");
-            inddTool.ExportPdfFromIndd(inddPath,""/*preset*/,pdfPath);
-        }
-        
     }
 }
