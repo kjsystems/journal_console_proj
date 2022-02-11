d106093dccfa2e955d4cdcd4886b004ec68259b1
Satoru Fujimori <satoru.fujimori@gmail.com>
2022-02-11 16:44:55 +0900

    kenkyu02を追加

---
+++ kenkyu02/MadoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kenkyu.lib.Model
+{
+	public class MadoItem
+	{
+		public int level {
+			get { return IsRight == true ? 0 : 1; } }		//KJ_TODO:反対
+		bool isleft_;
+		public bool IsLeft { get { return isleft_==true; } set { isleft_ = value; } }
+		public bool IsRight { get { return isleft_ != true; } set { isleft_ = value; } }
+		public int href { get; set; }			//ジャンプ先
+		public int Id { get; set; }
+
+        public string text { get; set; }
+		// public string text
+		// {
+		// 	get { return text_; }
+		// 	set { this.SetProperty(ref this.text_, value); }
+		// }
+	}
+}
---
+++ kenkyu02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using kj.kihon;
+using kenkyu.lib;
+
+namespace kenkyu02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new ErrorLogger("$error.txt");
+            if (log.isValid() != true) return;
+            try
+            {
+                Console.WriteLine($"{AppUtil.AppName}:入稿テキストからXMLの作成");
+                var lst = ArgListUtil.createArgList(args);
+                var srcdir = lst.getText('i');
+                srcdir.existDir();
+
+                Console.WriteLine("※TXTからXMLの作成のみ（研究Ｌのsql等は作成しない）");
+                var core = new kenkyu02_util(log);
+                //core.run(srcdir);
+                core.RunText2Xml(srcdir);
+            }
+            catch (Exception ex)
+            {
+                log.err("kenkyu02", ex.Message);
+            }
+            log.showdosmsg();
+        }
+    }
+}
